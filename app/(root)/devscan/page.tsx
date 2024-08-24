33303cbcbf27728897c173e19e4474673a2576f6
0xprathamesh <prathameshpatil6499@gmail.com>
2024-08-24 22:19:02 +0530

    styles

---
+++ app/(root)/devscan/page.tsx
@@ -0,0 +1,24 @@
+import Navbar from '@/components/Navbar'
+import Image from 'next/image'
+import React from 'react'
+import waves from "@/public/waves-light.svg";
+
+const Devscan = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className='h-60 w-full bg-[#727ff2] relative'>
+        <Image 
+          src={waves} 
+          alt='Waves' 
+          layout="fill" 
+          objectFit="cover" 
+          className='absolute inset-0' 
+        />
+        <p className='text-white text-center text-4xl pt-10'>DevScan Explorer</p>
+      </div>
+    </div>
+  )
+}
+
+export default Devscan;
