d36a0e90f5b8bd9e6781bd735f82e8a4e1b3a98b
0xprathamesh <prathameshpatil6499@gmail.com>
2024-09-02 21:05:25 +0530

    dashboard changes added

--- components/elements/Copyable.tsx
+++ components/elements/Copyable.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import toast from "react-hot-toast";
-import {HiOutlineDuplicate} from "react-icons/hi"
+import { HiOutlineDuplicate } from "react-icons/hi"
+
 type Props = {
     text: string
-    copyText?:string
+    copyText?: string
+    className?: string
 }
-const notify = () => toast.success("Copied To ClipBoard");
+
+const notify = () => toast.success("Copied To Clipboard");
 
 const Copyable = (props: Props) => {
+    const { text, copyText, className = '' } = props;
+
     const copyToClipboard = () => {
-        window.navigator.clipboard.writeText(props.copyText || props.text);
+        window.navigator.clipboard.writeText(copyText || text);
         notify();
     }
-    
-  return (
-    <span onClick={copyToClipboard} className='relative max-w-fit  group cursor-pointer  flex items-center gap-2 rounded-full px-2 py-1 bg-gray-100'>
-          <p className='dark-blue opacity-60'>{props.text}</p>
-          <HiOutlineDuplicate className="h-6 w-6 group-active:scale-95 text-gray-500" />
-    </span>
-  )
+
+    return (
+        <span 
+            onClick={copyToClipboard} 
+            className={`relative max-w-fit group cursor-pointer flex items-center gap-2 rounded-full px-2 py-1 ${className} `}
+        >
+            <p className='dark-blue opacity-60'>{text}</p>
+            <HiOutlineDuplicate className="h-4 w-4 group-active:scale-95 text-gray-500" />
+        </span>
+    )
 }
 
-export default Copyable
+export default Copyable;
